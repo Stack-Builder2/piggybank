e4379a3171e18ee212aa66d8e3cf638f254e171a
bsoupb <qotn0327@naver.com>
2025-08-23 14:42:18 +0900

    Feat: Payment 기초작업

--- src/main/java/com/example/piggybank/domain/payment/service/PaymentService.java
+++ src/main/java/com/example/piggybank/domain/payment/service/PaymentService.java
@@ -1,10 +1,5 @@
 package com.example.piggybank.domain.payment.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class PaymentService {
+public interface PaymentService {
 
 }
