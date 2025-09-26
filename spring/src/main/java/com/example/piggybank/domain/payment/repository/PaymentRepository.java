ee847d65538aca77a48a0eff410d7f3844872e40
OJaes <ojg1117@gmail.com>
2025-09-26 20:43:29 +0900

    Fix : v1 domain 삭제

--- spring/src/main/java/com/example/piggybank/domain/payment/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.piggybank.domain.payment.repository;
-
-import com.example.piggybank.domain.payment.entity.Payment;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-
-}
