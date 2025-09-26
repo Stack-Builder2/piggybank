5e321b2ffb26426a2a632df4b7347e344570fd7f
OJaes <ojg1117@gmail.com>
2025-09-26 20:06:20 +0900

    Fix: 거래내역 메서드 1차 수정

--- spring/src/main/java/com/example/piggybank/global/codef/event/CodefTranHistoryCreatedEvent.java
+++ spring/src/main/java/com/example/piggybank/global/codef/event/CodefTranHistoryCreatedEvent.java
@@ -1,13 +1,16 @@
 package com.example.piggybank.global.codef.event;
 
+import com.example.piggybank.accountmanagement.api.dto.response.FromCodefResponse;
 import com.example.piggybank.global.codef.dto.res.CodefTransactionResDto.TranHistory;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import lombok.Getter;
 
 @Getter
 public class CodefTranHistoryCreatedEvent extends CodefEvent{
     
+    private final CompletableFuture<FromCodefResponse> replyFuture;
     private final List<TranHistory> histories;
     private final UUID accountId;
     private final String balance;
@@ -18,5 +21,6 @@ public class CodefTranHistoryCreatedEvent extends CodefEvent{
         this.histories = histories;
         this.accountId = accountId;
         this.balance = balance;
+        replyFuture = new CompletableFuture<>();
     }
 }
